e4485b36becf866e30a9c9413af87ddde8cf358f
Simon Martin <smartin@scottlogic.com>
2020-02-18 08:40:24 +0000

    Fetch rust lang page, and extract links

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+extern crate select;
+
+use reqwest::Error;
+use select::document::Document;
+use select::predicate::Name;
+
+async fn fetch() -> Result<(), Error> {
+    let body = reqwest::get("https://www.rust-lang.org")
+        .await?
+        .text()
+        .await?;
+
+    Document::from(body.as_str())
+        .find(Name("a"))
+        .filter_map(|n| n.attr("href"))
+        .for_each(|x| println!("{}", x));
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    fetch().await?;
+    Ok(())
 }
